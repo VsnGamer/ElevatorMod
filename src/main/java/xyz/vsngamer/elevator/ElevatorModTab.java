c34b59abe7a4cf5641970bb51b04b0142ff81fc7
VsnGamer <vsngarcia5@gmail.com>
2016-12-06 19:28:19 +0000

    Added search bar to the Elevator Mod tab.

--- src/main/java/xyz/vsngamer/elevator/ElevatorModTab.java
+++ src/main/java/xyz/vsngamer/elevator/ElevatorModTab.java
@@ -7,16 +7,22 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.vsngamer.elevator.init.ModItems;
 
-public class ElevatorModTab extends CreativeTabs{
+public class ElevatorModTab extends CreativeTabs {
 
 	public ElevatorModTab() {
 		super("tabElevators");
+		setBackgroundImageName("item_search.png");
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public Item getTabIconItem() {
 		return ModItems.elevators.get(EnumDyeColor.WHITE);
+	}
+	
+	@Override
+	public boolean hasSearchBar() {
+		return true;
 	}
 
 }
