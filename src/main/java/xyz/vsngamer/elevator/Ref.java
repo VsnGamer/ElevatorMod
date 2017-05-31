05b56ff849b94ef2f5fac6c0987e53aba61b1d77
VsnGamer <vsngarcia5@gmail.com>
2017-05-31 18:24:59 +0100

    Version

--- src/main/java/xyz/vsngamer/elevator/Ref.java
+++ src/main/java/xyz/vsngamer/elevator/Ref.java
@@ -4,7 +4,7 @@ public class Ref {
 	
 	public static final String MOD_ID = "elevatorid";
 	public static final String NAME = "Elevator Mod";
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 	public static final String ACCPEPTED_VERSIONS = "[1.11.2]";
 	
 	public static final String CLIENT_PROXY_CLASS = "xyz.vsngamer.elevator.proxy.ClientProxy";
