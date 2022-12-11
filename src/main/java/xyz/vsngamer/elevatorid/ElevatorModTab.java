08d2f00d8318bf241938f58ada5754bcd4eedb95
Vasco <vsngarcia5@gmail.com>
2022-12-11 20:46:50 +0000

    Fix creative tab title

--- src/main/java/xyz/vsngamer/elevatorid/ElevatorModTab.java
+++ src/main/java/xyz/vsngamer/elevatorid/ElevatorModTab.java
@@ -1,5 +1,6 @@
 package xyz.vsngamer.elevatorid;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.DyeColor;
@@ -26,9 +27,11 @@ public class ElevatorModTab {
 
     @SubscribeEvent
     public static void register(CreativeModeTabEvent.Register event) {
-        TAB = event.registerCreativeModeTab(new ResourceLocation("elevators_tab", ElevatorMod.ID), builder -> builder
-                .icon(() -> new ItemStack(Registry.ELEVATOR_BLOCKS.get(DyeColor.WHITE).get()))
-                .displayItems((featureFlags, output, hasOp) -> Registry.ELEVATOR_ITEMS.values().forEach(o -> output.accept(o.get())))
+        TAB = event.registerCreativeModeTab(
+                new ResourceLocation("elevators_tab", ElevatorMod.ID), builder -> builder
+                        .icon(() -> new ItemStack(Registry.ELEVATOR_BLOCKS.get(DyeColor.WHITE).get()))
+                        .displayItems((featureFlags, output, hasOp) -> Registry.ELEVATOR_ITEMS.values().forEach(o -> output.accept(o.get())))
+                        .title(Component.translatable("itemGroup.elevators_tab"))
         );
     }
 }
