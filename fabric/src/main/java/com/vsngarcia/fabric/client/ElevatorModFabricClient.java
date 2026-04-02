260d401634f47a3046809586069c7cb3b449d55e
Vasco Garcia <vsngarcia5@gmail.com>
2026-04-02 22:08:54 +0100

    feat: Update to 26.1.1 & use new FRAPI

--- fabric/src/main/java/com/vsngarcia/fabric/client/ElevatorModFabricClient.java
+++ fabric/src/main/java/com/vsngarcia/fabric/client/ElevatorModFabricClient.java
@@ -1,17 +1,32 @@
 package com.vsngarcia.fabric.client;
 
 import com.vsngarcia.ElevatorHandler;
+import com.vsngarcia.ElevatorMod;
+import com.vsngarcia.client.ColorCamoElevator;
 import com.vsngarcia.client.gui.ElevatorScreen;
+import com.vsngarcia.fabric.ElevatorBlock;
+import com.vsngarcia.fabric.client.render.ElevatorBakedModel;
 import com.vsngarcia.level.ElevatorContainer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.model.loading.v1.ExtraModelKey;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier;
+import net.fabricmc.fabric.api.client.model.loading.v1.SimpleUnbakedExtraModel;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockColorRegistry;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.level.block.Block;
 
+import java.util.List;
+
+import static com.vsngarcia.fabric.FabricRegistry.ELEVATOR_BLOCKS;
 import static com.vsngarcia.fabric.FabricRegistry.ELEVATOR_CONTAINER;
 
 @Environment(EnvType.CLIENT)
@@ -20,8 +35,8 @@ public final class ElevatorModFabricClient implements ClientModInitializer {
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> ElevatorHandler.handleInput(ClientPlayNetworking::send));
 
-//        ColorProviderRegistry.BLOCK.register(new ColorCamoElevator(), ELEVATOR_BLOCKS.values().toArray(new Block[0]));
-//        ModelLoadingPlugin.register(new ElevatorModelLoadingPlugin());
+        BlockColorRegistry.register(List.of(new ColorCamoElevator()), ELEVATOR_BLOCKS.values().toArray(new Block[0]));
+        ModelLoadingPlugin.register(new ElevatorModelLoadingPlugin());
 
         MenuScreens.register(
                 ELEVATOR_CONTAINER,
@@ -34,25 +49,25 @@ public final class ElevatorModFabricClient implements ClientModInitializer {
         );
     }
 
-//    public static class ElevatorModelLoadingPlugin implements ModelLoadingPlugin {
-//
-//        public static final ExtraModelKey<BlockStateModel> ARROW_MODEL_KEY = ExtraModelKey.create(() -> "arrow");
-//        private static final Identifier ARROW_MODEL_ID = Identifier.fromNamespaceAndPath(ElevatorMod.ID, "arrow");
-//
-//        @Override
-//        public void initialize(Context ctx) {
-//            ctx.modifyBlockModelAfterBake().register(
-//                    ModelModifier.WRAP_PHASE, (model, context) -> {
-//                        if (!(context.state().getBlock() instanceof ElevatorBlock)) {
-//                            return model;
-//                        }
-//
-//                        ElevatorMod.LOGGER.debug("Wrapping elevator model: {}", context.state());
-//                        return new ElevatorBakedModel(model);
-//                    }
-//            );
-//
-//            ctx.addModel(ARROW_MODEL_KEY, SimpleUnbakedExtraModel.blockStateModel(ARROW_MODEL_ID));
-//        }
-//    }
+    public static class ElevatorModelLoadingPlugin implements ModelLoadingPlugin {
+
+        public static final ExtraModelKey<BlockStateModel> ARROW_MODEL_KEY = ExtraModelKey.create(() -> "arrow");
+        private static final Identifier ARROW_MODEL_ID = Identifier.fromNamespaceAndPath(ElevatorMod.ID, "arrow");
+
+        @Override
+        public void initialize(Context ctx) {
+            ctx.modifyBlockModelAfterBake().register(
+                    ModelModifier.WRAP_PHASE, (model, context) -> {
+                        if (!(context.state().getBlock() instanceof ElevatorBlock)) {
+                            return model;
+                        }
+
+                        ElevatorMod.LOGGER.debug("Wrapping elevator model: {}", context.state());
+                        return new ElevatorBakedModel(model);
+                    }
+            );
+
+            ctx.addModel(ARROW_MODEL_KEY, SimpleUnbakedExtraModel.blockStateModel(ARROW_MODEL_ID));
+        }
+    }
 }
--- fabric/src/main/java/com/vsngarcia/fabric/client/render/ElevatorBakedModel.java
+++ fabric/src/main/java/com/vsngarcia/fabric/client/render/ElevatorBakedModel.java
@@ -1,85 +1,98 @@
-//package com.vsngarcia.fabric.client.render;
-//
-//import com.vsngarcia.fabric.client.ElevatorModFabricClient;
-//import net.fabricmc.api.EnvType;
-//import net.fabricmc.api.Environment;
-//import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
-//import net.minecraft.client.Minecraft;
-//import net.minecraft.client.renderer.ItemBlockRenderTypes;
-//import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
-//import net.minecraft.client.renderer.block.model.BlockStateModelWrapper;
-//import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
-//import net.minecraft.core.BlockPos;
-//import net.minecraft.core.Direction;
-//import net.minecraft.util.RandomSource;
-//import net.minecraft.world.level.BlockAndTintGetter;
-//import net.minecraft.world.level.block.state.BlockState;
-//import org.joml.Vector3f;
-//
-//import java.util.function.Predicate;
-//
-//import static com.vsngarcia.fabric.ElevatorBlock.DIRECTIONAL;
-//import static com.vsngarcia.fabric.ElevatorBlock.SHOW_ARROW;
-//import static net.minecraft.world.level.block.HorizontalDirectionalBlock.FACING;
-//
-//@Environment(EnvType.CLIENT)
-//public class ElevatorBakedModel extends BlockStateModelWrapper {
-//    private static final Minecraft MC = Minecraft.getInstance();
-//
-//    public ElevatorBakedModel(BlockStateModel originalModel) {
-//        super(originalModel);
-//    }
-//
-//    @Override
-//    public void emitQuads(
-//            QuadEmitter emitter,
-//            BlockAndTintGetter blockView,
-//            BlockPos pos,
-//            BlockState state,
-//            RandomSource random,
-//            Predicate<Direction> cullTest
-//    ) {
-//        if (state != null && state.getValue(DIRECTIONAL) && state.getValue(SHOW_ARROW)) {
-//            emitter.pushTransform(quad -> {
-//                Vector3f vec = null;
-//                for (int i = 0; i < 4; i++) {
-//                    vec = quad.copyPos(i, vec);
-//                    vec.sub(.5f, 0, .5f);
-//                    vec.rotateY((float) Math.toRadians(-state.getValue(FACING).toYRot()));
-//                    vec.add(.5f, 0, .5f);
-//
-//                    quad.pos(i, vec);
-//                }
-//
-//                quad.renderLayer(ChunkSectionLayer.CUTOUT);
-//
-//                return true;
-//            });
-//
-//
-//            var arrow = MC.getModelManager().getModel(ElevatorModFabricClient.ElevatorModelLoadingPlugin.ARROW_MODEL_KEY);
-//            if (arrow != null) {
-//                arrow.emitQuads(emitter, blockView, pos, state, random, cullTest);
-//            }
-//
-//            emitter.popTransform();
-//        }
-//
-//        if (blockView.getBlockEntityRenderData(pos) instanceof BlockState heldState) {
-//            emitter.pushTransform(quad -> {
-//                quad.renderLayer(ItemBlockRenderTypes.getChunkRenderType(heldState));
-//
-//                return true;
-//            });
-//
-//            MC.getBlockRenderer()
-//                    .getBlockModel(heldState)
-//                    .emitQuads(emitter, blockView, pos, heldState, random, cullTest);
-//
-//            emitter.popTransform();
-//            return;
-//        }
-//
-//        super.emitQuads(emitter, blockView, pos, state, random, cullTest);
-//    }
-//}
+package com.vsngarcia.fabric.client.render;
+
+import com.vsngarcia.fabric.ElevatorBlock;
+import com.vsngarcia.fabric.client.ElevatorModFabricClient;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.model.loading.v1.wrapper.WrapperBlockStateModel;
+import net.fabricmc.fabric.api.client.renderer.v1.mesh.QuadEmitter;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
+import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import net.minecraft.client.resources.model.sprite.Material;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
+import org.joml.Vector3f;
+
+import java.util.function.Predicate;
+
+import static com.vsngarcia.fabric.ElevatorBlock.DIRECTIONAL;
+import static com.vsngarcia.fabric.ElevatorBlock.SHOW_ARROW;
+import static net.minecraft.world.level.block.HorizontalDirectionalBlock.FACING;
+
+@Environment(EnvType.CLIENT)
+public class ElevatorBakedModel extends WrapperBlockStateModel {
+    private static final Minecraft MC = Minecraft.getInstance();
+
+    public ElevatorBakedModel(BlockStateModel originalModel) {
+        super(originalModel);
+    }
+
+    @Override
+    public void emitQuads(
+            QuadEmitter emitter,
+            BlockAndTintGetter blockView,
+            BlockPos pos,
+            BlockState state,
+            RandomSource random,
+            Predicate<Direction> cullTest
+    ) {
+        if (!(state.getBlock() instanceof ElevatorBlock)) {
+            super.emitQuads(emitter, blockView, pos, state, random, cullTest);
+            return;
+        }
+
+
+        if (state.getValue(DIRECTIONAL) && state.getValue(SHOW_ARROW)) {
+            emitter.pushTransform(quad -> {
+                Vector3f vec = null;
+                for (int i = 0; i < 4; i++) {
+                    vec = quad.copyPos(i, vec);
+                    vec.sub(.5f, 0, .5f);
+                    vec.rotateY((float) Math.toRadians(-state.getValue(FACING).toYRot()));
+                    vec.add(.5f, 0, .5f);
+
+                    quad.pos(i, vec);
+                }
+
+                quad.chunkLayer(ChunkSectionLayer.CUTOUT);
+
+                return true;
+            });
+
+
+            var arrow = MC.getModelManager().getModel(ElevatorModFabricClient.ElevatorModelLoadingPlugin.ARROW_MODEL_KEY);
+            if (arrow != null) {
+                arrow.emitQuads(emitter, blockView, pos, state, random, cullTest);
+            }
+
+            emitter.popTransform();
+        }
+
+        if (blockView.getBlockEntityRenderData(pos) instanceof BlockState heldState) {
+            MC.getModelManager()
+                    .getBlockStateModelSet()
+                    .get(heldState)
+                    .emitQuads(emitter, blockView, pos, heldState, random, cullTest);
+            return;
+        }
+
+        super.emitQuads(emitter, blockView, pos, state, random, cullTest);
+    }
+
+    @Override
+    public Material.Baked particleMaterial(BlockAndTintGetter level, BlockPos pos, BlockState state) {
+        if (level != BlockAndTintGetter.EMPTY
+                && level.getBlockEntityRenderData(pos) instanceof BlockState heldState) {
+            return MC.getModelManager()
+                    .getBlockStateModelSet()
+                    .get(heldState)
+                    .particleMaterial(level, pos, heldState);
+        }
+
+        return super.particleMaterial(level, pos, state);
+    }
+}
